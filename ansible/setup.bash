6523aea6976236e05d7cdb526fc6ced84d0c7dc9
John Harwell <john.r.harwell@gmail.com>
2022-07-07 15:04:19 -0500

    Commit version used for PhD thesis experiments!

--- ansible/setup.bash
+++ ansible/setup.bash
@@ -5,4 +5,3 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/.local/lib
 export LOG4CXX_CONFIGURATION=$HOME/research/fordyca/log4cxx.xml
 export ROS_HOSTNAME=$(hostname -I| tr -d '[:space:]')
 export ROS_IP=$(hostname -I| tr -d '[:space:]')
-
